7a614001490993d9bf29737b8b604cecb88ec04f
Korimer <thekclov@gmail.com>
2026-08-09 15:54:22 -0600

    fixed list concat

--- module/default.nix
+++ module/default.nix
@@ -1,21 +1,24 @@
 let
-  genDeclaration = prop: val: "${prop}: ${val};\n";
+  genDeclaration = prop: val: "${prop}: ${val};";
   genSelectors = selectors: builtins.concatStringsSep " " selectors;
 
   selectorText = cssAttrs:
     genSelectors cssAttrs.selectors;
+
   declarationText = cssAttrs:
-    map
-      (dec: genDeclaration dec.property dec.value)
-      cssAttrs.declarations;
+    builtins.concatStringsSep "\n"
+      (map
+        (dec: genDeclaration dec.property dec.value)
+        cssAttrs.declarations);
 
   ruleText = cssAttrs: ''
     ${selectorText cssAttrs} {
       ${declarationText cssAttrs}
     }
   '';
-  
-  genFullCSS = cssArr: builtins.concatStringsSep "\n" (map ruleText cssArr);
+
+  genFullCSS = cssArr:
+    builtins.concatStringsSep "\n" (map ruleText cssArr);
 in
 {
   generate = genFullCSS;
